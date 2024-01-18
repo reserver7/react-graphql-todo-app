3daf263ded73c14abfb480793dcfdd717b91f52b
reserver7 <reserver7@gmail.com>
2024-01-18 15:59:22 +0900

    env 파일 설정

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: process.env.VERCEL_ENV === 'production'
-        ? process.env.APOLLO_SERVER_URL
+    uri: import.meta.env.VERCEL_ENV === 'production'
+        ? import.meta.env.APOLLO_SERVER_URL
         : 'http://localhost:3001/',
     cache: new InMemoryCache(),
 });
