fe1ebece04f85e06ad7fdd969f316c4917c35817
reserver7 <reserver7@gmail.com>
2024-01-18 15:38:36 +0900

    apollo uri 수정

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://localhost:3001/',
+    uri: 'http://localhost:3001/' || 'https://react-graphql-todo-app.vercel.app/',
     cache: new InMemoryCache(),
 })
 
